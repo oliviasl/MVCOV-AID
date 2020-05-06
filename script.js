473d6613f323ea2f5806766262f21e999ce9f99f
wangkath <56166245+wangkath@users.noreply.github.com>
2020-05-06 13:09:47 -0700

    navbar updates

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("nav").style.height = "54px";
+    document.getElementById("header").style.fontSize = "40px";
   } else {
-    document.getElementById("nav").style.height = "78px";
+    document.getElementById("nav").style.height = "85px";
+    document.getElementById("header").style.fontSize = "60px";
   }
 }
 
